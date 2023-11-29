c289bdbf0d2e29cbaa59526fb278ee02b7f81d81
Alka <ajiju.2000@hotmail.com>
2023-11-29 16:50:01 -0600

    Se muestran los datos de la tabla

--- Unidad2/Enlace.js
+++ Unidad2/Enlace.js
@@ -33,8 +33,45 @@ let dataBindMethod = function(datos){
             }
 };
 
+function mostrarTr(event){
+    let a = event.target;
+    let inicio = parseInt(a.dataset.inicio);
+    let final = parseInt(a.dataset.fin);
+
+    let rows = Array.from(a.closest("table").querySelectorAll("tbody tr"));
+
+    for (let i = inicio; i <= final; i++) {
+        rows[i].hidden = false;
+        
+    }
+}
+
+let paginaMethod = function(tamanoPagina){
+    //Calcular enlaces
+    let totalTr = this.querySelectorAll("tbody tr").length;
+    let totalPag = Math.ceil(totalTr / tamanoPagina);
+    //Crear el tfoot
+    let tfoot = document.createElement("tfoot");
+    let tfootTr = tfoot.insertRow();
+    let tfootTd = tfootTr.insertCell();
+    for (let i = 1; i <= totalPag; i++) {
+        let a = document.createElement("a");
+        a.textContent = i;
+        a.href="#";
+        a.dataset.inicio = (i -1)*tamanoPagina;
+        a.dataset.fin = parseInt(a.dataset.inicio) + tamanoPagina -1;
+        a.onclick= mostrarTr;
+        tfootTd.append(a);
+    }
+    tfootTd.colSpan = this.tBodies[0].rows[0].cells.length;
+    this.append(tfoot);
+};
+
+
+
 document.querySelectorAll("table").forEach(x=> {
     x.dataBind = dataBindMethod;
     x.filter = filterMethod;
+    x.paginate = paginaMethod;
 });
      
